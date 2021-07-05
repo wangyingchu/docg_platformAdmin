d363e5691304af3fcd6160cf0c1c019b58b3ae79
wangychu <wangyc-j@glodon.com>
2021-07-05 09:55:34 +0800

    init setup

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -10,7 +10,7 @@ import org.vaadin.artur.helpers.LaunchUtil;
  */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-//
+
     public static void main(String[] args) {
         LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
     }
